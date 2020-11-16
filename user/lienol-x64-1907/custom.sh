9a42b790cf9881a0d891dbc912a0b02862cec4de
左丽楠 <952802194@qq.com>
2020-11-16 20:34:14 +0800

    update custom.sh

--- user/lienol-x64-1907/custom.sh
+++ user/lienol-x64-1907/custom.sh
@@ -4,4 +4,5 @@ echo "Test custom.sh"
 
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
 git clone https://github.com/tty228/luci-app-serverchan.git       package/luci-app-serverchan
-
+git clone https://github.com/sensec/luci-app-udp2raw.git          package/luci-app-udp2raw
+git clone https://github.com/sensec/openwrt-udp2raw.git           package/openwrt-udp2raw
